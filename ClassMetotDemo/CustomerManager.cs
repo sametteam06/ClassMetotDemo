92369131318bf0712e5b8db5692053a9a01a69cb
sametteam06 <ozdemir.abdulsamet@outlook.com>
2021-01-19 22:35:22 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            customer.IsActive = true;
+            Console.WriteLine(customer.Name + " isimli müşteri eklendi.");
+            Console.WriteLine("-----------------------");
+        }
+        public void ShowAll(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                if (customer.IsActive == true)
+                {
+                    Console.WriteLine("Müşteri Adı : " + customer.Name);
+                    Console.WriteLine("Müşteri Adı : " + customer.Surname);
+                    Console.WriteLine("-----------------------");
+                }
+            }
+        }
+        public void Delete(Customer customer)
+        {
+            customer.IsActive = false;
+            Console.WriteLine(customer.Name + " isimli müşteri silindi.");
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Name1";
+            customer1.Surname = "Surname1";
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Name2";
+            customer2.Surname = "Surname2";
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Name3";
+            customer3.Surname = "Surname3";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1); //customer1 eklendi ve ekrana ismi yazıldı.
+
+            customerManager.ShowAll(customers); //eklenen müşteriler listelendi (sadece customer1 listelenir).
+
+            customerManager.Add(customer2); //customer2 eklendi.
+
+            customerManager.ShowAll(customers); //eklenen müşteriler listelendi (customer1 ve costomer2 listelenir).
+
+            customerManager.Add(customer3); //customer3 eklendi.
+
+            customerManager.ShowAll(customers); //eklenen müşteriler listelendi (customer1, customer2 ve customer3 listelenir).
+        }
+    }
+}
